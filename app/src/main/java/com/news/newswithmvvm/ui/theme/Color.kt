053a401bc6579eebf3dcd34f03787abff0e315c3
SecurFlyInfinity <securflyinfinity@gmail.com>
2023-09-28 23:42:39 +0530

    added theme colors

--- app/src/main/java/com/news/newswithmvvm/ui/theme/Color.kt
+++ app/src/main/java/com/news/newswithmvvm/ui/theme/Color.kt
@@ -9,3 +9,17 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+
+// -- Light Colors --
+val card = Color(0xFFFFFFFF)
+val background = Color(0xFFB7ADCC)
+val primary = Color(0xFF208F5B)
+val text = Color(0xFF141414)
+
+
+// -- Night Colors --
+val cardNight = Color(0xFF272727)
+val backgroundNight = Color(0xFF34353A)
+val primaryNight = Color(0xFF208F5B)
+val textNight = Color(0xFFF3F3F3)
